50801ae78b88826791071568724f3d9157595550
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-22 13:14:00 +0800

    assign env with func

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
+const assignEnv = require('./lib/assignEnv');
 const parseConfig = require('./lib/parseConfig');
 const loadConfigFile = require('./lib/loadConfigFile');
 const resolveEntryConfFile = require('./lib/resolveEntryConfFile');
 const locateConfDir = require('./lib/locateConfDir');
 const locateProjRootDir = require('./lib/locateProjRootDir');
 
-
-module.exports = Object.assign(
-  {},
-  parseConfig(
-    loadConfigFile(
-      resolveEntryConfFile(
-        locateConfDir(
-          locateProjRootDir(
-            module.parent.filename || process.cwd()
+module.exports =
+  assignEnv(
+    parseConfig(
+      loadConfigFile(
+        resolveEntryConfFile(
+          locateConfDir(
+            locateProjRootDir(
+              module.parent.filename || process.cwd()
+            )
           )
         )
       )
     )
-  ),
-  { env: process.env.NODE_ENV }
-);
+  );
