e88fe7e8e4381ecc90e312b8a38c300d01f62bfa
Zhang Kaiyu <yeannylam@gmail.com>
2019-05-10 14:32:14 +0800

    resolve dynamic files

--- test/any-file/test.js
+++ test/any-file/test.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = '';
 
 test('automatically find \'2conf.json\' in \'conf\' dir', () => {
-  expect(require('./app.js')).toEqual({"env": "2conf", "key": "var"});
+  expect(require('./app.js')).toEqual({ 'env': '2conf','key': 'var' });
 });
